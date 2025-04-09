d9c1625290dd4cfd526a29ad23b567464b842281
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-04-09 23:35:50 +0200

    Started with validation.

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,6 +6,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValue.email !== "" && !enteredValue.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log("User Email: ", enteredValue.email);
@@ -33,6 +36,9 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValue.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
